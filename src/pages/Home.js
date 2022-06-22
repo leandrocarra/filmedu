05780e20923783654bd2a97361f93cffdcb63020
Alfinete <leandrocarra@gmail.com>
2022-06-22 13:52:52 -0300

    add: services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,3 @@
-import React from 'react'
-import PosterCard from '../components/PosterCard'
+import Home from '../containers/home';
 
-import '../style/home.scss'
-
-const Home = () => {
-  return (
-    <div className="home__content">
-      <h2 className="home__title">Filmes para sua familia</h2>
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-      <PosterCard />
-    </div>
-  );
-}
- 
-export default Home
+export default Home;
