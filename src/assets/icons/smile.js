f8f6872c67cb2cdaa52dfa77f3798a1338118d47
Alfinete <leandrocarra@gmail.com>
2022-06-24 19:14:51 -0300

    add : mock

---
+++ src/assets/icons/smile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function smile() {
+  return (
+    <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.043457" y="0.587524" width="40" height="40" rx="5" fill="#F2077C"/>
+      <rect x="24.3547" y="11.8495" width="3.00525" height="9.86762" rx="1.50263" fill="white"/>
+      <rect x="31.0999" y="28.029" width="3.00525" height="20.8559" rx="1.50263" transform="rotate(90 31.0999 28.029)" fill="white"/>
+      <rect x="13.0015" y="11.8495" width="3.00525" height="9.86762" rx="1.50263" fill="white"/>
+      <rect x="9.55054" y="25.0204" width="3.00525" height="6.01385" rx="1.50263" fill="white"/>
+      <rect x="28.0945" y="25.0204" width="3.00525" height="6.01385" rx="1.50263" fill="white"/>
+    </svg>
+  )
+}
