a58556ecaf6605db0887730696a75b22c180e0d0
Alfinete <leandrocarra@gmail.com>
2022-06-25 01:24:20 -0300

    add: features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import Routes from './routes'
 
-import history from './services/history';
+import history from './services/history'
 
 function App() {
   return (
@@ -14,4 +14,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
